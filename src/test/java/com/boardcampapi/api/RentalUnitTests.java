039b57f59371891ca5dbc3326f9f0f8b0f50963d
Lucas Ajaj Piccoli <lucasajaj@gmail.com>
2024-02-09 18:13:09 -0300

    Implemented unit test for updating rental on Rental Service

--- src/test/java/com/boardcampapi/api/RentalUnitTests.java
+++ src/test/java/com/boardcampapi/api/RentalUnitTests.java
@@ -19,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.boardcampapi.api.dtos.RentalDTO;
 import com.boardcampapi.api.exceptions.CustomerNotFoundException;
 import com.boardcampapi.api.exceptions.GameNotFoundException;
+import com.boardcampapi.api.exceptions.RentalNotFoundException;
 import com.boardcampapi.api.exceptions.UnprocessableRentalException;
 import com.boardcampapi.api.models.CustomerModel;
 import com.boardcampapi.api.models.GameModel;
@@ -230,4 +231,71 @@ class RentalUnitTests {
         verify(rentalRepository, times(1)).save(any());
         assertEquals(newRental, result);
     }
+
+    @Test
+    void givenWrongRentalId_whenUpdatingRental_thenThrowsError() {
+        //given
+        Long id = 1L;
+
+        doReturn(Optional.empty()).when(rentalRepository).findById(any());
+
+        //when
+        RentalNotFoundException exception = assertThrows(
+            RentalNotFoundException.class,
+            () -> rentalService.update(id)
+        );
+
+        //then
+        verify(rentalRepository, times(1)).findById(any());
+        verify(rentalRepository, times(0)).save(any());
+        assertNotNull(exception);
+        assertEquals("Rental not found!", exception.getMessage());
+    }
+
+    @Test
+    void givenCloseRental_whenUpdatingRental_thenThrowsError() {
+        //given
+        Long id = 1L;
+        LocalDate returnDate = LocalDate.of(2024,1,5);
+        GameModel game = new GameModel(1L, "Game", "http://", 5L, 1500L);
+        CustomerModel customer = new CustomerModel(1L, "Customer", "12345678910");
+        RentalModel rental = new RentalModel(id,LocalDate.of(2024,1,1),3L,returnDate,4500L,0L,customer,game);
+
+        doReturn(Optional.of(rental)).when(rentalRepository).findById(any());
+
+        //when
+        UnprocessableRentalException exception = assertThrows(
+            UnprocessableRentalException.class,
+            () -> rentalService.update(id)
+        );
+
+        //then
+        verify(rentalRepository, times(1)).findById(any());
+        verify(rentalRepository, times(0)).save(any());
+        assertNotNull(exception);
+        assertEquals("This rental is already closed!", exception.getMessage());
+    }
+
+    @Test
+    void givenValidRental_whenUpdatingRental_thenUpdatesRental() {
+        //given
+        Long id = 1L;
+        LocalDate returnDate = null;
+        GameModel game = new GameModel(1L, "Game", "http://", 5L, 1500L);
+        CustomerModel customer = new CustomerModel(1L, "Customer", "12345678910");
+        RentalModel rental = new RentalModel(id,LocalDate.of(2024,1,1),3L,returnDate,4500L,0L,customer,game);
+        RentalModel newRental = new RentalModel(id,LocalDate.of(2024,1,1),3L,LocalDate.of(2024,1,5),4500L,1500L,customer,game);
+
+        doReturn(Optional.of(rental)).when(rentalRepository).findById(any());
+        doReturn(newRental).when(rentalRepository).save(any());
+
+        //when
+        RentalModel result = rentalService.update(id);
+
+        //then
+        verify(rentalRepository, times(1)).findById(any());
+        verify(rentalRepository, times(1)).save(any());
+        assertEquals(newRental, result);
+    }
+
 }
